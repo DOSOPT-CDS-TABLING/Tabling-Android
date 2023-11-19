c6193b55202c4b960001061b3faa96ee804069af
jihyunniiii <jita1125@naver.com>
2023-11-20 01:35:40 +0900

    [chore] #1 ViewModelFactory 수정

--- app/src/main/java/org/sopt/tabling/presentation/common/ViewModelFactory.kt
+++ app/src/main/java/org/sopt/tabling/presentation/common/ViewModelFactory.kt
@@ -1,13 +1,12 @@
 package org.sopt.tabling.presentation.common
 
-import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import org.sopt.tabling.data.datasource.remote.DummyDataSource
 import org.sopt.tabling.data.repository.DummyRepositoryImpl
 import org.sopt.tabling.presentation.dummy.DummyViewModel
 
-class ViewModelFactory(private val context: Context) : ViewModelProvider.Factory {
+class ViewModelFactory : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(DummyViewModel::class.java)) {
             return DummyViewModel(DummyRepositoryImpl(DummyDataSource())) as T
--- app/src/main/java/org/sopt/tabling/presentation/dummy/DummyActivity.kt
+++ app/src/main/java/org/sopt/tabling/presentation/dummy/DummyActivity.kt
@@ -1,14 +1,14 @@
 package org.sopt.tabling.presentation.dummy
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import org.sopt.tabling.R
 import org.sopt.tabling.databinding.ActivityDummyBinding
+import org.sopt.tabling.presentation.common.ViewModelFactory
 import org.sopt.tabling.util.binding.BindingActivity
 
 class DummyActivity : BindingActivity<ActivityDummyBinding>(R.layout.activity_dummy) {
-    private val dummyViewModel: DummyViewModel by viewModels<DummyViewModel>()
+    private val dummyViewModel: DummyViewModel by viewModels { ViewModelFactory() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
