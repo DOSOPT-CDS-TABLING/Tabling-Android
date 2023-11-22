a1fb3d40c8d490862e605440f2c1809b0899e3c9
Do-Re-Min-Woo <kimy5878@naver.com>
2023-11-23 03:59:48 +0900

    [add] #3 초기 파일 추가

---
+++ app/src/main/java/org/sopt/tabling/presentation/waiting/WaitingDetailActivity.kt
@@ -0,0 +1,12 @@
+package org.sopt.tabling.presentation.waiting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.sopt.tabling.R
+
+class WaitingDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_waiting_detail)
+    }
+}
