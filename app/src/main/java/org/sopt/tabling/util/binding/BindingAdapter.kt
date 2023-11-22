f535d2d1b34e6363cb9d463418e35dd26571ce31
Do-Re-Min-Woo <kimy5878@naver.com>
2023-11-23 02:07:05 +0900

    [feat] #3 Binding Adpater ImageView Coil

---
+++ app/src/main/java/org/sopt/tabling/util/binding/BindingAdapter.kt
@@ -0,0 +1,10 @@
+package org.sopt.tabling.util.binding
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import coil.load
+
+@BindingAdapter("image")
+fun ImageView.setImage(imageUrl: String) {
+    this.load(imageUrl)
+}
