1b7a1102a325a31814642de534b90fdab6d79efd
DoReMinWoo <kimy5878@naver.com>
2023-11-22 17:09:12 +0900

    [feat] #3 DataBinding + Adapter 생성 및 연결

--- app/src/main/java/org/sopt/tabling/presentation/store/PopularStoreActivity.kt
+++ app/src/main/java/org/sopt/tabling/presentation/store/PopularStoreActivity.kt
@@ -2,6 +2,7 @@ package org.sopt.tabling.presentation.store
 
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.lifecycle.Observer
 import org.sopt.tabling.R
 import org.sopt.tabling.databinding.ActivityPopularStoreBinding
 import org.sopt.tabling.util.binding.BindingActivity
@@ -9,9 +10,25 @@ import org.sopt.tabling.util.binding.BindingActivity
 class PopularStoreActivity :
     BindingActivity<ActivityPopularStoreBinding>(R.layout.activity_popular_store) {
     private val storeViewModel by viewModels<StoreViewModel>()
+    private lateinit var popularStoreAdapter: PopularStoreAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.storeViewModel = storeViewModel
+
+        storeViewModel.setDummyList()
+        initAdapter()
+        setPopularStoreList()
+    }
+
+    private fun initAdapter() {
+        popularStoreAdapter = PopularStoreAdapter(this)
+        binding.rvPopularStore.adapter = popularStoreAdapter
+    }
+
+    private fun setPopularStoreList() {
+        storeViewModel.popularStoreList.observe(this, Observer { storeList ->
+            popularStoreAdapter.setStoreList(storeList)
+        })
     }
 }
