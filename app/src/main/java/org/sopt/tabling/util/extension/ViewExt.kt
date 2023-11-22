3e7279dfc4de0c0cd734685697fde6b687cd0a87
jihyunniiii <jita1125@naver.com>
2023-11-23 04:51:22 +0900

    [feat] #4 ratingBar 기능 구현

--- app/src/main/java/org/sopt/tabling/util/extension/ViewExt.kt
+++ app/src/main/java/org/sopt/tabling/util/extension/ViewExt.kt
@@ -2,8 +2,17 @@ package org.sopt.tabling.util.extension
 
 import android.view.View
 import com.google.android.material.snackbar.Snackbar
+import org.sopt.tabling.databinding.ViewRatingBarBinding
 
 fun View.showSnackbar(message: String, isShort: Boolean = true) {
     val duration = if (isShort) Snackbar.LENGTH_SHORT else Snackbar.LENGTH_LONG
     Snackbar.make(this, message, duration).show()
+}
+
+fun ViewRatingBarBinding.setRatingBar(rating: Float) {
+    ivRatingBarFirst.isEnabled = (1 <= rating)
+    ivRatingBarSecond.isEnabled = (2 <= rating)
+    ivRatingBarThird.isEnabled = (3 <= rating)
+    ivRatingBarFourth.isEnabled = (4 <= rating)
+    ivRatingBarFifth.isEnabled = (5 <= rating)
 }
