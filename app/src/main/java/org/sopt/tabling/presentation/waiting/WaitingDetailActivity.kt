92bf7766df5c8cbc6fe41f3fc62c46be692d82ee
Do-Re-Min-Woo <kimy5878@naver.com>
2023-11-24 00:38:54 +0900

    [feat] #6 DataBinding 연결

--- app/src/main/java/org/sopt/tabling/presentation/waiting/WaitingDetailActivity.kt
+++ app/src/main/java/org/sopt/tabling/presentation/waiting/WaitingDetailActivity.kt
@@ -1,12 +1,18 @@
 package org.sopt.tabling.presentation.waiting
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import org.sopt.tabling.R
+import org.sopt.tabling.databinding.ActivityWaitingDetailBinding
+import org.sopt.tabling.util.binding.BindingActivity
 
-class WaitingDetailActivity : AppCompatActivity() {
+class WaitingDetailActivity :
+    BindingActivity<ActivityWaitingDetailBinding>(R.layout.activity_waiting_detail) {
+    private val waitingViewModel by viewModels<WaitingViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_waiting_detail)
+
+        binding.waitingData = waitingViewModel.waitingDetail.value
     }
 }
