8a8e052457d5e26456db0f8dadaca279affd32ae
Do-Re-Min-Woo <kimy5878@naver.com>
2023-11-24 00:38:26 +0900

    [add] #6 DummyData 추가

--- app/src/main/java/org/sopt/tabling/presentation/waiting/WaitingViewModel.kt
+++ app/src/main/java/org/sopt/tabling/presentation/waiting/WaitingViewModel.kt
@@ -1,4 +1,27 @@
 package org.sopt.tabling.presentation.waiting
 
-class WaitingViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import org.sopt.tabling.data.model.response.ResponseWaitingDetailDto
+
+class WaitingViewModel : ViewModel() {
+    private val _waitingDetail =
+        MutableLiveData<ResponseWaitingDetailDto.WaitingDetailData>()
+    val waitingDetail : LiveData<ResponseWaitingDetailDto.WaitingDetailData> = _waitingDetail
+
+    init {
+        _waitingDetail.value =
+            ResponseWaitingDetailDto.WaitingDetailData(
+                1,
+                "파이브가이즈 여의도",
+                66,
+                4,
+                "2023-11-21 (화) 11:23",
+                2,
+                "이용 예정",
+                0,
+                "맛있어요",
+            )
+    }
 }
