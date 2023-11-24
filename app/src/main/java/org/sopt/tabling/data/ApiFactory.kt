0994046a693d62cf0bfedce8eca21ffdb6c4a3e9
jihyunniiii <jita1125@naver.com>
2023-11-25 02:29:55 +0900

    [init] #9 서버통신 관련 기초세팅 (base url, ApiFactory)

---
+++ app/src/main/java/org/sopt/tabling/data/ApiFactory.kt
@@ -0,0 +1,30 @@
+package org.sopt.tabling.data
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.sopt.tabling.BuildConfig
+import retrofit2.Retrofit
+
+object ApiFactory {
+    private val client by lazy {
+        OkHttpClient.Builder().addInterceptor(HttpLoggingInterceptor().apply {
+            level =
+                if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
+        }).build()
+    }
+    val retrofit: Retrofit by lazy {
+        Retrofit.Builder().baseUrl(BuildConfig.BASE_URL)
+            .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+            .client(client).build()
+    }
+
+
+    inline fun <reified T> create(): T = retrofit.create<T>(T::class.java)
+}
+
+object ServicePool {
+
+}
