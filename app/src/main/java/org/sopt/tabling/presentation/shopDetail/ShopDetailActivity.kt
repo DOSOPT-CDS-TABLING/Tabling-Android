bd8fec941a979d498b1c736764789ade705c087d
jihyunniiii <jita1125@naver.com>
2023-11-30 03:44:22 +0900

    [chore] #24 ktlint 적용

--- app/src/main/java/org/sopt/tabling/presentation/shopDetail/ShopDetailActivity.kt
+++ app/src/main/java/org/sopt/tabling/presentation/shopDetail/ShopDetailActivity.kt
@@ -181,12 +181,12 @@ class ShopDetailActivity :
         setTvShopDetailShopImgPageText(FIRST_POSITION)
 
         binding.vpShopDetailShopImg.registerOnPageChangeCallback(object :
-            ViewPager2.OnPageChangeCallback() {
-            override fun onPageSelected(position: Int) {
-                super.onPageSelected(position)
-                setTvShopDetailShopImgPageText(position)
+                ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    setTvShopDetailShopImgPageText(position)
+                }
             }
-        }
         )
     }
 
